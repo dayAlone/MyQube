054a780732b21aaae0b86ac06bc37924a01120c0
dayAlone <andrey.slider@gmail.com>
2016-01-13 20:09:22 +0500

    CSS

--- group/u_creative/teaser.php
+++ group/u_creative/teaser.php
@@ -2,12 +2,12 @@
   <div class="contest__content">
     <h4>Творческие союзы, принявшие участие в проекте U_Concept, создали свои работы. Пришла ваша очередь проявить себя. </h4>
     <div class="row contest__teaser">
-      <div class="col-xs-12 col-sm-6">
+      <div class="col-xs-12 col-md-6">
           <img src="/group/u_creative/images/teaser-icon-1.png" class="hidden-xs hidden-sm" alt="" width="32"/>
           <img src="/group/u_creative/images/teaser-icon-1-big.png" class="hidden-md hidden-lg" alt=""/>
           <br/><span>Авторизуйтесь на сайте и ответьте на вопросы онлайн викторины U_Creative**.</span>
       </div>
-      <div class="col-xs-12 col-sm-6">
+      <div class="col-xs-12 col-md-6">
           <img src="/group/u_creative/images/teaser-icon-2.png" class="hidden-xs hidden-sm" alt="" width="40"/>
           <img src="/group/u_creative/images/teaser-icon-2-big.png" class="hidden-md hidden-lg" alt=""/>
           <br/><span>У каждого участника есть шанс получить свитшот с принтом собственного дизайна.</span>
@@ -23,7 +23,14 @@
     ** Ю_Креатив
 </div>
 <style media="screen">
+    div.main section {
+        margin-top: -30px !important;
+    }
+    .body {
+        min-width: 0 !important;
+    }
     div.main {
         padding-left: 0 !important;
+        padding-top: 0;
     }
 </style>
