d5262607183de825ced2371d8f9acf6cd9410a45
dayAlone <andrey.slider@gmail.com>
2016-01-13 20:00:48 +0500

    Teaser

--- group/u_creative/teaser.php
+++ group/u_creative/teaser.php
@@ -1,9 +1,29 @@
-<div class="contest contest--teaser center">
+<div class="contest contest--teaser">
   <div class="contest__content">
     <h4>Творческие союзы, принявшие участие в проекте U_Concept, создали свои работы. Пришла ваша очередь проявить себя. </h4>
     <div class="row contest__teaser">
-      <div class="col-xs-12 col-sm-6"><img src="/group/u_creative/images/teaser-icon-1.png" alt="" width="32"/><br/><span>Авторизуйтесь на сайте и ответьте на вопросы онлайн викторины U_Creative**.</span></div>
-      <div class="col-xs-12 col-sm-6"><img src="/group/u_creative/images/teaser-icon-2.png" alt="" width="40"/><br/><span>У каждого участника есть шанс получить свитшот с принтом собственного дизайна.</span></div>
-    </div><a href="/group/1/?backurl=/group/1/contest/u_creative/" class="button">авторизация</a>
+      <div class="col-xs-12 col-sm-6">
+          <img src="/group/u_creative/images/teaser-icon-1.png" class="hidden-xs hidden-sm" alt="" width="32"/>
+          <img src="/group/u_creative/images/teaser-icon-1-big.png" class="hidden-md hidden-lg" alt=""/>
+          <br/><span>Авторизуйтесь на сайте и ответьте на вопросы онлайн викторины U_Creative**.</span>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+          <img src="/group/u_creative/images/teaser-icon-2.png" class="hidden-xs hidden-sm" alt="" width="40"/>
+          <img src="/group/u_creative/images/teaser-icon-2-big.png" class="hidden-md hidden-lg" alt=""/>
+          <br/><span>У каждого участника есть шанс получить свитшот с принтом собственного дизайна.</span>
+      </div>
+    </div>
+    <div class='center'>
+        <a href="/group/1/?backurl=/group/1/contest/u_creative/" class="button">авторизация</a>
+    </div>
   </div>
 </div>
+<div class='contest-info right'>
+    * ю_концепт<br/>
+    ** Ю_Креатив
+</div>
+<style media="screen">
+    div.main {
+        padding-left: 0 !important;
+    }
+</style>
