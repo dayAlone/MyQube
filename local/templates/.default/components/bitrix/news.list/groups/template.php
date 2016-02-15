c6d1dfe34733cd90b7cefdc5a025dd059555b832
dayAlone <andrey.slider@gmail.com>
2016-02-15 23:28:06 +0500

    Template

--- local/templates/.default/components/bitrix/news.list/groups/template.php
+++ local/templates/.default/components/bitrix/news.list/groups/template.php
@@ -1,6 +1,6 @@
 <div class="groups">
 	<?foreach ($arResult['ITEMS'] as $key => $item) {?>
-		<a href="#" class="groups__item">
+		<a href="<?=$item['DETAIL_PAGE_URL']?>" class="groups__item">
 			<div style="background-image: url(<?=urlencode($item['PREVIEW_PICTURE']['SRC'])?>)" class="groups__image"></div>
 			<div class="groups__name"><?=$item['NAME']?></div>
 			<div class="groups__users"><img src="/layout/images/svg/people.svg" alt=""><span><?=$item['PROPERTIES']['USERS'][VALUE]?></span> <?=pluralize($item['PROPERTIES']['USERS'][VALUE], array('участников', 'участников', 'участник', 'участника'))?></div>
