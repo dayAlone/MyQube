0368805c922882c3ee26856a2087007b27dde262
dayAlone <andrey.slider@gmail.com>
2016-02-15 23:22:52 +0500

    template

--- local/templates/.default/components/bitrix/news.list/groups/template.php
+++ local/templates/.default/components/bitrix/news.list/groups/template.php
@@ -1,7 +1,7 @@
 <div class="groups">
 	<?foreach ($arResult['ITEMS'] as $key => $item) {?>
 		<a href="#" class="groups__item">
-			<div style="background-image: url(<?=$item['PREVIEW_PICTURE']['SRC']?>)" class="groups__image"></div>
+			<div style="background-image: url(<?=urlencode($item['PREVIEW_PICTURE']['SRC'])?>)" class="groups__image"></div>
 			<div class="groups__name"><?=$item['NAME']?></div>
 			<div class="groups__users"><img src="/layout/images/svg/people.svg" alt=""><span><?=$item['PROPERTIES']['USERS'][VALUE]?></span> <?=pluralize($item['PROPERTIES']['USERS'][VALUE], array('участников', 'участников', 'участник', 'участника'))?></div>
 			<div class="groups__lock"><img src="/layout/images/svg/lock-group.svg" alt=""><br><span class="hidden-xs">Это закрытая<br/> группа</span></div>
