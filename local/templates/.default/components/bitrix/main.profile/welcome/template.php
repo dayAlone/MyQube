a5b3ed3effb9659ae14937ad8c01196af1741a6d
dayAlone <andrey.slider@gmail.com>
2016-02-18 16:35:15 +0500

    Welcome

--- local/templates/.default/components/bitrix/main.profile/welcome/template.php
+++ local/templates/.default/components/bitrix/main.profile/welcome/template.php
@@ -28,7 +28,7 @@
 	   <a href="/user/groups/" class="welcome__action action">
 
 	   Группы
-	   <div class="action__counter"><?=(count($user['UF_GROUPS']) > 0 ? count($user['UF_GROUPS']) : '')?></div><img src="/layout/images/svg/blue-arrow.svg" class="action__arrow"></a>
+	   <div class="action__counter"><?=($user['UF_GROUPS'] && count($user['UF_GROUPS']) > 0 ? count($user['UF_GROUPS']) : '')?></div><img src="/layout/images/svg/blue-arrow.svg" class="action__arrow"></a>
 	 <div class="row">
 	   <div class="col-xs-6 left"><a href="/user/profile/?edit=y" class="welcome__link welcome__link--settings"><?=svg('settings')?>Настройки</a></div>
 	   <div class="col-xs-6 right"><a href="/?logout=yes" class="welcome__link"><?=svg('menu-exit')?>Выйти</a></div>
